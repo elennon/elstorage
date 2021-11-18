1298c03bff204d877c63b6ee2211f089007f10cd
elennon <elennon@outlook.ie>
2021-11-18 22:26:31 +0000

    firxed php

--- forms/contact2.php
+++ forms/contact2.php
@@ -3,27 +3,30 @@
     ini_set("include_path", '/home/e587261/php:' . ini_get("include_path") );
     require_once "Mail.php";
  
-    $visitor_name = htmlspecialchars($_POST['visitor_name']);
-    $visitor_email = htmlspecialchars($_POST['visitor_email']);
-    $email_title = htmlspecialchars($_POST['visitor_title']);
-    $email_body = htmlspecialchars($_POST['email_body']); 
-    $recipient = "eee_lennon@yahoo.com";
- 
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $title = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']); 
+    $recipient = "info@elstorage.ie";
+
+ $body = "Name: $name\nEmail: $email\n\nMessage:\n$message\n\nRegards,\n$name";
+ $subject = "From: $name <$email> $title";
  $host = "91.210.235.216";
  $username = "info@elstorage.ie";
  $password = "Rhiabit666";
  
  $headers = array ('From' => "info@elstorage.ie",
    'To' => $recipient,
-   'Subject' => $email_title);
+   'Subject' => $subject);
  $smtp = Mail::factory('smtp',
    array ('host' => $host,
      'auth' => "PLAIN",
      'socket_options' => array('ssl' => array('verify_peer_name' => false)),
      'username' => $username,
      'password' => $password));
+
  
- $mail = $smtp->send($recipient, $headers, $email_body);
+ $mail = $smtp->send($recipient, $headers, $body);
  
  if (PEAR::isError($mail)) {
    echo("<p>" . $mail->getMessage() . "</p>");
